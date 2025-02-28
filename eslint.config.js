cc76fc2326831519a69f94ae1b3c82f2f169c3b8
kornilowds <v.kornilowd@gmail.com>
2025-02-28 10:13:38 +0200

    Added prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,22 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+import pluginPrettier from 'eslint-plugin-prettier/recommended'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
     // Globally ignored files
     ignores: [
-      "**/.eslintrc.cjs",
-      "**/*.config.js",
-      "**/*.config.cjs",
-      "dist",
-      "pnpm-lock.yaml",
-      "**/build",
-      "**/public/build",
-      "**/playwright-report",
+      '**/.eslintrc.cjs',
+      '**/*.config.js',
+      '**/*.config.cjs',
+      'dist',
+      'pnpm-lock.yaml',
+      '**/build',
+      '**/public/build',
+      '**/playwright-report',
     ],
   },
 
@@ -24,17 +25,19 @@ export default [
   pluginReact.configs.flat.recommended,
 
   {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
     languageOptions: {
       globals: globals.browser,
     },
     settings: {
       react: {
-        version: "detect",
+        version: 'detect',
       },
     },
     rules: {
-      "react/react-in-jsx-scope": "off",
+      'react/react-in-jsx-scope': 'off',
     },
   },
-];
+
+  pluginPrettier,
+]
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-import Inspect from "vite-plugin-inspect";
+import { vitePlugin as remix } from '@remix-run/dev'
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import Inspect from 'vite-plugin-inspect'
 
-declare module "@remix-run/node" {
+declare module '@remix-run/node' {
   interface Future {
-    v3_singleFetch: true;
+    v3_singleFetch: true
   }
 }
 
@@ -23,4 +23,4 @@ export default defineConfig({
     }),
     tsconfigPaths(),
   ],
-});
+})
