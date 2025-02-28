27b5a71dbb64de92c0e4b00bdd8cddcf99e6a73a
kornilowds <v.kornilowd@gmail.com>
2025-02-28 09:46:58 +0200

    Changed ESlint to v9

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    // Globally ignored files
+    ignores: [
+      "**/.eslintrc.cjs",
+      "**/*.config.js",
+      "**/*.config.cjs",
+      "dist",
+      "pnpm-lock.yaml",
+      "**/build",
+      "**/public/build",
+      "**/playwright-report",
+    ],
+  },
+
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+];
